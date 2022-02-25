0c14bf0826c6b1c67b6c2208da89208c7c65190c
Jean-Christophe Paradis <jparadis@c1r2p6.42quebec.com>
2022-02-25 12:15:24 -0500

    testgithub

---
+++ ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jparadis <marvin@42quebec.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/24 16:56:04 by jparadis          #+#    #+#             */
+/*   Updated: 2022/02/24 17:09:24 by jparadis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba\n", 27);
+}
