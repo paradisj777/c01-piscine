83d3edb2112f47a49acbcf69e72440c859985513
root <root@DESKTOP-8M5D05R.localdomain>
2022-03-02 17:07:33 -0500

    ft_ultimate_ft

---
+++ c01/ft_ultimate_ft.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void	ft_ultimate_ft()
+{
+	int a;
+	int *nbr;
+	int **nbr2;
+	int ***nbr3;
+	int ****nbr4;
+	int *****nbr5;
+	int ******nbr6;
+	int *******nbr7;
+	int ********nbr8;
+	int *********nbr9;	
+}
+
+
+
+
+int main(void)
+{
+	int a;
+	int *nbr;
+
+	a = 42;
+	*nbr = &a;
+void	ft_ultimate_ft(int *********nbr);
+	return(0);
+}	
